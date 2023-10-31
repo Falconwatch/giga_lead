34d1340f0e34d861badfb301f87063ec4806c671
Daniil <dfenelonov@yandex.ru>
2023-10-31 18:32:41 +0300

    add tests

---
+++ test/test_newshandler.py
@@ -0,0 +1,24 @@
+from giga_lead.news_leadgen.dto.newsdto import NewsDTO
+from giga_lead.news_leadgen.news_handler import NewsHandler
+
+from unittest import main, IsolatedAsyncioTestCase
+
+class Test(IsolatedAsyncioTestCase):
+    async def test_process_one_news(self):
+        nh = NewsHandler()
+        one_news = NewsDTO(news_id="123",text = "текст новости")
+        response = await nh.process_news(one_news)
+        self.assertEqual(tuple, type(response))
+
+    async def test_process_batch_of_news(self):
+        nh = NewsHandler()
+        all_news = [
+            NewsDTO(news_id='aaaa', text='В Москве произошла крупная авария'),
+            NewsDTO(news_id='bbbb', text='В Москве произошла крупная авария'),
+            NewsDTO(news_id='cccc', text='В Москве произошла крупная авария')
+        ]
+        response = await nh.batch_process_news(all_news)
+        self.assertEqual(list, type(response))
+
+
+
