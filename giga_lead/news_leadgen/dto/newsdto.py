86764e0a3b0d3c35ba70cd7929c8d85a9d5dc56b
Daniil <dfenelonov@yandex.ru>
2023-10-31 14:37:39 +0300

    adding async

--- giga_lead/news_leadgen/dto/newsdto.py
+++ giga_lead/news_leadgen/dto/newsdto.py
@@ -1,20 +1,20 @@
 from pydantic import BaseModel
-
+from typing import Optional
 
 class NewsDTO(BaseModel):
-    news_id: str
-    text: str 
+    news_id: Optional[str] = None
+    text: Optional[str] = None
 
 class ProcessedNewsDTO(BaseModel):
     news_id: str
     processing_result: list[str]
 
 class ServiceRequestDTO(BaseModel):
-    user_uid: str | None = None
-    service_uid: str |None = None
+    user_uid: Optional[str] = None
+    service_uid: Optional[str] = None
     news: list[NewsDTO]
 
 class ServiceResponseDTO(BaseModel):
-    user_uid: str | None = None
-    service_uid: str |None = None
+    user_uid: Optional[str] = None
+    service_uid: Optional[str] = None
     processed_news: list[ProcessedNewsDTO]
