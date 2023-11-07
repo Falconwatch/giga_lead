24a51e49f76d4d9fa0e722ca6e57dd8c5b044aac
AlanSl13 <alansl@mail.ru>
2023-11-07 14:25:00 +0300

    create endpoint /upload

--- giga_lead/web_app.py
+++ giga_lead/web_app.py
@@ -16,32 +16,32 @@ nh._giga_call("Моё сообщение")
 async def ping():
     return "pong"
 
-
-@app.post("/news/batch") # Это эндпоинт
+@app.post("/news/batch")
 async def process_batch_news(batch_news: ServiceRequestDTO) -> ServiceResponseDTO:
     batch_news_response = [nh.process_news(one_news.text) for one_news in batch_news.news]
     if batch_news_response & len(batch_news_response):
         return jsonable_encoder(batch_news_response)
     return HTTPException(400, detail="Something went wrong")
 
-@app.post("/file/")
-async def create_upload_file(file: UploadFile = File(...)):
-    return {"filename": file.filename}
 
 @app.post("/upload")
-def upload_file(file: UploadFile = File(...)):
-    from news_leadgen.news_handler import NewsHandler
-    
-    news_batch = pd.read_excel(file.file)[:3]
-    news_processing_batch = nh.batch_process_news(news_batch)
-    npb_list = list()
-    for npr in news_processing_batch:
-        npb_list.append((npr[1], npr[0].news_id, npr[0].processing_result))
+async def upload_file(file: UploadFile = File(...)):
+    ''' эндпоинт который принимает на вход csv файл, проверяет корректные данные, форматирует ввод'''
+    news_batch = pd.read_csv(file.file, sep = ';')
 
-    responses = pd.DataFrame(data=npb_list, columns=["Заданные вопросы", "ID новости", "Ответы на вопросы"])
-    
+    print(news_batch.columns.values.tolist())
+    # Проверяем, что колонки соответствуют названию
+    mask = ['ID', 'text']
+    if news_batch.columns.values.tolist() != mask:
+        return HTTPException(406, detail="Wrong name of columns")
+
+    #Переводим все колонки в стринг:
+
+    for i in news_batch.columns:
+        news_batch[i] = news_batch[i].astype(str)
+            
     file.file.close()
-    return file.filename, responses.head()
+    return HTTPException(200, detail="Upload successfully")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=3003)
