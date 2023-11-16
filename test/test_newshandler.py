abbce687d6d3b1949e54f1d3725bee2430bf3fd3
AlanSl13 <alansl@mail.ru>
2023-11-16 11:22:56 +0300

    Добавлен  ассерт на длину входа = выхода

--- test/test_newshandler.py
+++ test/test_newshandler.py
@@ -37,7 +37,9 @@ async def test_process_batch_of_news():
     all_news = all_news * 10
 
     response = await nh.batch_process_news(all_news)
+    k = 0
+    for npr in response:
+        k +=1
+
+    assert k == len(all_news), 'Length of Enter is not equal to Exit length'
     assert(list, type(response))
-
-
-
